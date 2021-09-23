4a6fd3ebbcb0cce2d648ea7a558ce1e575bac044
Brandon Mojica <61305029+BrannMojLop@users.noreply.github.com>
2021-09-23 11:19:49 -0500

    Delete env.bash

--- env.bash
+++
@@ -1,4 +0,0 @@
-export NODE_ENV="development"
-export MONGODB_URI="mongodb+srv://brann:brandonmojica18@bedu-backend.kfbjk.mongodb.net/income-system?retryWrites=true&w=majority"
-export PORT="4001"
-export SECRET="secret"
