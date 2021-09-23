e99b8453c174e5af652a28eb5b245ba4fb366193
Brandon Mojica <brandonmojica95@gmail.com>
2021-09-23 11:56:16 -0500

    validando database

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ require('./config/passport');
 import server from "./config/server";
 
 
-console.log("Database_URL", process.env.DATABASE_URL);
+console.log("Database_URL", process.env.MONGODB_URI);
 
 const main = async () => {
     await server.listen(server.get('port'));
