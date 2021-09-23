0fd202dea9f4ee9b70f4920fdac1d1fbc4a99d99
Brandon Mojica <61305029+BrannMojLop@users.noreply.github.com>
2021-09-23 10:02:48 -0500

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 export async function connect() {
-    try {
         const db = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
         });
         return db;
-    } catch (err) {
-        console.log(err);
     }
 }
 
