e016f5fa3ab8073a49f4f6cf8981e7038b15d95e
Brandon Mojica <brann@Lenovo-ideapad-330S-15IKB-7af8e8eb.localdomain>
2021-09-22 21:18:04 -0500

    Eliminando accesos sencibles en el codigo

---
+++ src/config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV="development"
+export PORT=4000
+export SECRET="secret"
+export MONGODB_URI_DEV="mongodb+srv://brann:sbaSIEr2SHs7VPw7@bedu-backend.kfbjk.mongodb.net/income-system?retryWrites=true&w=majority"
