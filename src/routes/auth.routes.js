15d5a328319d30efd50821c7207f2a1141ecb449
Brandon Mojica <61305029+BrannMojLop@users.noreply.github.com>
2021-09-23 14:29:07 -0500

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const jwt = require('express-jwt');
-const secret = require('../config/index.js').secret;
+const secret = require('../config').secret;
 
 
 // Obtenemos el jwt del header de la petición y verificamos su existencia.
