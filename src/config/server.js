af96b5658ed5f60e432fb27f4080d9f3702eaf72
Brandon Mojica <brandonmojica95@gmail.com>
2021-10-16 13:44:20 -0500

    Agregando configuracion de Cors para peticion Ajax

--- src/config/server.js
+++ src/config/server.js
@@ -21,6 +21,14 @@ server.set("port", process.env.PORT || 4001);
 
 // Middleware
 server.use(express.json());
+// Configurar cabeceras y cors
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Routes
 server.use(IndexRoute);
