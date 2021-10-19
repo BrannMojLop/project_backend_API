7d7d28bc59c1610f7bab81cbb552a75c10e37047
Brandon Mojica <brandonmojica95@gmail.com>
2021-10-18 21:45:50 -0500

    Reparando API

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 export async function connect(res) {
-  try {
-    const db = await mongoose.connect( process.env.MONGODB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    );
-    return db;
-  } catch (e) {
-    return res.status(422).send(err);
-  }
+    try {
+        const db = await mongoose.connect("mongodb+srv://brann:brandonmojica18@bedu-backend.kfbjk.mongodb.net/income-system-prod?retryWrites=true&w=majority", {
+            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+        });
+        return db;
+    } catch (e) {
+        return res.status(422).send(err)
+    }
+
 }
 
 module.exports = connect;
--- src/controllers/periods.js
+++ src/controllers/periods.js
@@ -1,302 +1,188 @@
 const connect = require('../config/database');
-const Publication = require('../models/Publication');
+const Period = require('../models/Period');
 const User = require('../models/User');
-const Product = require('../models/Product');
-const Category = require('../models/Category');
 
-async function showPublications(req, res) {
+async function showPeriods(req, res) {
     await connect();
-    if (req.query.title) {
 
-        const search = new RegExp(`${req.query.title}`, 'i');
-        await Publication.aggregate([
-            {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'id_product',
-                    'foreignField': '_id',
-                    'as': 'product'
-                }
-            }, {
-                '$match': {
-                    'title': search
-                }
+    if (req.body.require || req.body.limit) {
+        if (!req.body.limit) {
+            req.body.limit = Infinity;
+        } else if (!req.body.require) {
+            req.body.require = {
+                _id: 1,
+                name: 1,
+                days: 1,
+                createdAt: 1,
+                status: 1,
+                updatedAt: 1
             }
+        }
+    } else {
+        req.body.require = {
+            _id: 1,
+            name: 1,
+            days: 1,
+            createdAt: 1,
+            status: 1,
+            updatedAt: 1
+        }
+        req.body.limit = Infinity;
+    }
 
-        ], function (err, publications) {
+    if (req.query.name) {
+        await Period.aggregate([
+            {
+                '$project': req.body.require
+            },
+            {
+                '$limit': req.body.limit
+            }, {
+                '$match': { "name": { $regex: req.query.name, $options: "$i" } }
+            }
+        ], function (err, periods) {
             if (err) {
                 res.status(401).send(err);
-            } else if (publications.length > 0) {
-                res.status(200).send(publications);
+            } else if (periods.length > 0) {
+                res.status(200).send(periods);
             } else {
-                res.status(404).send("No se han encontrado registros");
-            }
-        })
-    } else if (req.query.sector) {
-
-        const publications = await Publication.aggregate([
-            {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'id_product',
-                    'foreignField': '_id',
-                    'as': 'product'
-                }
-            }], function (err, publications) {
-                if (err) {
-                    res.status(401).send(err);
-                } else {
-                    return publications
-                }
-            }
-        );
-
-        const categories = await Category.aggregate([
-            {
-                '$lookup': {
-                    'from': 'sectors',
-                    'localField': 'id_sector',
-                    'foreignField': '_id',
-                    'as': 'sector'
-                }
-            }], function (err, categories) {
-                if (err) {
-                    res.status(401).send(err);
-                } else {
-                    return categories
-                }
-            });
-
-        function searchPublications(publications, categories) {
-            const listCategories = []
-            const results = [];
-            categories.forEach(category => {
-                if (category.sector[0]._id == req.query.sector) {
-                    listCategories.push(String(category._id))
-                }
-            })
-
-            for (let index = 0; index < listCategories.length; index++) {
-                publications.filter(publication => {
-                    if (publication.product[0].id_category == listCategories[index]) {
-                        results.push(publication)
-                    }
-                })
-            }
-
-            if (publications.length <= 0) {
-                res.status(404).send("No se han encontrado registros");
-            } else {
-                res.status(200).send(publications);
-            }
-        }
-
-        searchPublications(publications, categories)
-
-    } else if (req.query.category) {
-
-        await Publication.aggregate([
-            {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'id_product',
-                    'foreignField': '_id',
-                    'as': 'product'
-                }
-            }], function (err, publications) {
-                const search = publications.filter(publication => {
-                    if (publication.product[0].id_category == req.query.category) {
-                        return publication;
-                    }
-                })
-                if (err) {
-                    res.status(401).send(err);
-                } else if (search.length > 0) {
-                    res.status(200).send(search);
-                } else {
-                    res.status(404).send("No se han encontrado registros");
-                }
-            }
-        );
-    } else if (req.query.min_price || req.query.max_price) {
-        if (!req.query.min_price) {
-            req.query.min_price = 0
-        }
-        if (!req.query.max_price) {
-            req.query.max_price = Infinity
-        }
-        await Publication.aggregate([
-            {
-                '$match': {
-                    'prices': {
-                        '$gte': Number(req.query.min_price),
-                        '$lte': Number(req.query.max_price)
-                    }
-                }
-            }, {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'id_product',
-                    'foreignField': '_id',
-                    'as': 'product'
-                }
-            }
-        ], function (err, publications) {
-            if (err) {
-                res.status(401).send(err);
-            } else {
-                if (err) {
-                    res.status(401).send(err);
-                } else if (publications.length > 0) {
-                    res.status(200).send(publications);
-                } else {
-                    res.status(404).send("No se han encontrado registros");
-                }
+                res.status(204).send("No se han encontrado registros");
             }
         })
     } else {
-        await Publication.aggregate([
+        await Period.aggregate([
             {
-                '$lookup': {
-                    'from': 'products',
-                    'localField': 'id_product',
-                    'foreignField': '_id',
-                    'as': 'product'
-                }
-            }], function (err, publications) {
-                if (err) {
-                    res.send(err);
-                } else {
-                    res.status(200).send(publications);
-                }
+                '$project': req.body.require
+            },
+            {
+                '$limit': req.body.limit
             }
-        );
-    }
-}
-
-
-async function createPublication(req, res) {
-    const publication = new Publication(req.body)
-
-    await connect();
-
-    const user = await User.findById(req.usuario.id);
-    const type = await user.typeUser(user.id_type);
-
-    if (type === 2) {
-        await publication.save(function (err) {
+        ], function (err, periods) {
             if (err) {
-                res.status(400).json({
-                    success: false,
-                    type: err.title,
-                    error: err.message
-                });
+                res.status(401).send(err);
+            } else if (periods.length > 0) {
+                res.status(200).send(periods);
             } else {
-                res.status(201).json({
-                    success: "Publicacion creada con Exito",
-                    Publication: publication
-                });
+                res.status(204).send("No se han encontrado registros");
             }
-        });
-    } else {
-        res.status(401).send("Permisos insuficientes")
+        })
     }
 }
 
+async function createPeriod(req, res) {
+    const period = new Period(req.body)
 
-async function getPublication(req, res) {
-    await connect();
-    try {
-        const publication = await Publication.findById(req.params.id);
-        if (!publication) {
-            res.status(204).send("No se han encontrado registros");
-        } else {
-            res.status(200).send(publication);
-        }
-    } catch (err) {
-        res.status(400).send(err)
-    }
-}
-
-async function updatePublication(req, res) {
-    await connect();
-
-    const user = await User.findById(req.usuario.id);
-    const type = await user.typeUser(user.id_type);
-
-    if (type === 2) {
-        const publication = await Publication.findById(req.params.id, function (err) {
-            if (err) {
-                res.status(400).json({
-                    error: err.name,
-                    message: err.message
-                })
-            }
-        });
-        const product = await Product.findById(publication.id_product);
-        if (!publication) {
-            res.status(204).send("No se han encontrado el registro");
-        } else if (product.id_lessor != user.id) {
-            res.status(401).send("Permisos insuficientes");
-        } else {
-            await Publication.findByIdAndUpdate(req.params.id, {
-                $set: req.body
-            });
-            res.status(200).send({
-                message: 'Publicacion Actualizada con Exito'
-            });
-        }
-    } else {
-        res.status(401).send("Permisos insuficientes")
-    }
-}
-
-async function disablePublication(req, res) {
-    await connect();
-
-    const user = await User.findById(req.usuario.id);
-    const type = await user.typeUser(user.id_type);
-
-    if (type === 2) {
-        const publication = await Publication.findById(req.params.id, function (err) {
-            if (err) {
-                res.status(400).json({
-                    error: err.name,
-                    message: err.message
-                })
-            }
-        });
-        const product = await Product.findById(publication.id_product);
-        if (!publication) {
-            res.status(204).send("No se han encontrado el registro");
-        } else if (product.id_lessor != user.id) {
-            res.status(401).send("Permisos insuficientes");
-        } else {
-            await Publication.findByIdAndUpdate(req.params.id, {
-                "status": false
-            });
-            res.status(200).send({
-                message: 'Publicacion Deshabilitada con Exito'
-            });
-        }
-    } else {
-        res.status(401).send("Permisos insuficientes")
-    }
-}
-
-async function disablePublications(req, res) {
     await connect();
 
     const user = await User.findById(req.usuario.id);
     const type = await user.typeUser(user.id_type);
 
     if (type === 1) {
-        await Publication.updateMany({ "status": false }, function (err, publications) {
+        await period.save(function (err) {
+            if (err) {
+                res.status(400).json({
+                    success: false,
+                    type: err.name,
+                    error: err.message
+                });
+            } else {
+                res.status(201).json({
+                    success: "Periodo creado con Exito",
+                    Period: period
+                });
+            }
+        });
+    } else {
+        res.status(401).send("Permisos insuficientes")
+    }
+}
+
+
+async function getPeriod(req, res) {
+    await connect();
+    try {
+        const period = await Period.findById(req.params.id);
+        if (!period) {
+            res.status(204).send("No se han encontrado registros");
+        } else {
+            res.status(200).send(period);
+        }
+    } catch (err) {
+        res.status(400).send(err)
+    }
+}
+
+async function updatePeriod(req, res) {
+    await connect();
+
+    const user = await User.findById(req.usuario.id);
+    const type = await user.typeUser(user.id_type);
+
+    if (type === 1) {
+        const period = await Period.findById(req.params.id, function (err) {
+            if (err) {
+                res.status(400).json({
+                    error: err.name,
+                    message: err.message
+                })
+            }
+        });
+        if (!period) {
+            res.status(401).send("No se han encontrado el registro");
+        } else {
+            await Period.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            });
+            res.status(200).send({
+                message: 'Periodo Actualizado con Exito'
+            });
+        }
+    } else {
+        res.status(401).send("Permisos insuficientes")
+    }
+}
+
+async function disablePeriod(req, res) {
+    await connect();
+
+    const user = await User.findById(req.usuario.id);
+    const type = await user.typeUser(user.id_type);
+    if (type === 1) {
+        const period = await Period.findById(req.params.id, function (err) {
+            if (err) {
+                res.status(400).json({
+                    error: err.name,
+                    message: err.message
+                })
+            }
+        });
+        if (!period) {
+            res.status(401).send("No se han encontrado el registro");
+        } else {
+            await Period.findByIdAndUpdate(req.params.id, {
+                "status": false
+            });
+            res.status(200).send({
+                message: 'Periodo Deshabilitado con Exito'
+            });
+        }
+    } else {
+        res.status(401).send("Permisos insuficientes")
+    }
+}
+
+async function disablePeriods(req, res) {
+    await connect();
+
+    const user = await User.findById(req.usuario.id);
+    const type = await user.typeUser(user.id_type);
+    if (type === 1) {
+        await Period.updateMany({ "status": false }, function (err, periods) {
             if (err) {
                 res.status(401).send("No se han encontrado el registros");
             } else {
                 res.status(200).send({
-                    message: 'Publicaciones Deshabilitadas con Exito'
+                    message: 'Periodos Deshabilitados con Exito'
                 });
             }
         });
@@ -307,10 +193,10 @@ async function disablePublications(req, res) {
 
 // exportamos las funciones definidas
 module.exports = {
-    createPublication,
-    showPublications,
-    getPublication,
-    disablePublication,
-    updatePublication,
-    disablePublications
+    createPeriod,
+    showPeriods,
+    getPeriod,
+    disablePeriod,
+    updatePeriod,
+    disablePeriods
 }
